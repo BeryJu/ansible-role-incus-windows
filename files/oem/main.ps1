16422b65855653e0118668f153700e8e0641ec30
Jens Langhammer <jens.langhammer@beryju.org>
2026-03-21 16:40:44 +0100

    cleanup poewrshell v1

--- files/oem/main.ps1
+++ files/oem/main.ps1
@@ -1,3 +1,8 @@
+Function Log {
+    $Message = $args[0]
+    Write-Output $Message
+}
+
 get-wmiobject win32_logicaldisk | % {
 	if ($_.volumename -like 'STUFF*') {
 		$setupdrive = $_.deviceid
@@ -5,14 +10,22 @@ get-wmiobject win32_logicaldisk | % {
 }
 
 cmd.exe /c "${setupdrive}\OEM\compile-dotnet-assemblies.bat"
-reg.exe add "HKLM\System\CurrentControlSet\Control\Network\NewNetworkWindowOff"
 
-. "${setupdrive}\OEM\power.ps1"
-# QEMU guest-agent
-. "${setupdrive}\OEM\qemu-ga.ps1"
+Log "Settings - Disable 'New network' window"
+reg.exe add "HKLM\System\CurrentControlSet\Control\Network\NewNetworkWindowOff"
+Log "Settings - High performance power plan"
+powercfg -setactive scheme_min
+Log "Settings - Disable hibernate file"
+cmd /c "%systemroot%\System32\reg.exe ADD HKLM\SYSTEM\CurrentControlSet\Control\Power\ /v HibernateFileSizePercent /t REG_DWORD /d 0 /f"
+cmd /c "%systemroot%\System32\reg.exe ADD HKLM\SYSTEM\CurrentControlSet\Control\Power\ /v HibernateEnabled /t REG_DWORD /d 0 /f"
+
+Log "Drivers - Installing qemu Agent"
+Start-Process msiexec.exe -Wait -ArgumentList ("/I ${setupdrive}\guest-agent\qemu-ga-x86_64.msi /quiet")
+Log "Drivers - Installing qemu Guest Additions"
+Start-Process msiexec.exe -Wait -ArgumentList ("/I ${setupdrive}\virtio-win-gt-x64.msi /quiet")
 start-sleep 30
 
-# Install incus-agent
+Log "Install incus-agent"
 $agentdrive = Get-WmiObject -Class Win32_Volume | Where-Object { $_.Label -eq "incus-agent" }
 if ($agentdrive -and (Test-Path -Path "$($agentdrive.Name)install.ps1")) {
 	. "$($agentdrive.Name)install.ps1"
@@ -26,7 +39,5 @@ start-sleep 30
 if (test-path "${setupdrive}\local\main.ps1") {
 	. "${setupdrive}\local\main.ps1"
 }
-
-#msiexec /i "${setupdrive}\CloudbaseInitSetup_Stable_x64.msi" /qn
 
 cmd.exe /c "${setupdrive}\OEM\sysprep.bat" "${setupdrive}\OEM\unattend.xml"
