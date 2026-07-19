c7ed9b725eaa2752ab80a9da1ad60de274104677
Philippe Grégoire <git@pgregoire.xyz>
2026-07-19 13:12:02 -0400

    sysprep typo

--- oem/sysprep.bat
+++ oem/sysprep.bat
@@ -2,7 +2,7 @@ if exist C:\script.bat del C:\script.bat
 
 mkdir %WINDIR%\Setup\Scripts
 
-rem Disable WinRM when until Windows is not fully initialized / started
+rem Disable WinRM when until Windows is fully initialized / started
 netsh advfirewall firewall set rule name="Allow WinRM HTTPS" new action=block
 netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new action=block
 echo netsh advfirewall firewall set rule name="Allow WinRM HTTPS" new action=allow ^>^> %%WINDIR%%\Temp\SetupComplete.log >> %WINDIR%\Setup\Scripts\SetupComplete.cmd
